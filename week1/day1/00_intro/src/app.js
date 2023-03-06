2d16a7c7e04956e93eb8aee2fc81ff4dee631400
Jackie <kong123890@gmail.com>
2023-03-06 12:21:34 -0800

    finished assignment

--- week1/day1/00_intro/src/app.js
+++ week1/day1/00_intro/src/app.js
@@ -114,8 +114,36 @@ const landingPage =
 
 // translate jsx code to react.createElement code.
 // code here 
+let landingPage1 = React.createElement(
+    "div",
+    {className: "container"},
+    React.createElement("nav", null, React.createElement("ul", null, 
+        React.createElement("li", null, React.createElement("h3", null, "LOGO")),
+        React.createElement("li", null, React.createElement("a", {href: "#home"}, "Home")),
+        React.createElement("li", null, React.createElement("a", {href: "#about"}, "About")),
+        React.createElement("li", null, React.createElement("a", {href: "#contact"}, "Contact")))
+    ),
+    React.createElement("main", null, 
+        React.createElement("section", null, 
+            React.createElement("h1", {id: "title"}, "Hello World"),
+            React.createElement("p", {id: "paragraph"}, "This is first React Application"),
+            React.createElement("button", {className: "btn btn-dark"}, "Click me")),
+        React.createElement("section", {id: "article"}, 
+            React.createElement("article", null, 
+                React.createElement("h3", null, "Item One"),
+                React.createElement("p", null, "Nulla amet fugiat nostrud sint nulla aute est. ")),
+            React.createElement("article", null, 
+                React.createElement("h3", null, "Item Two"),
+                React.createElement("p", null, "Nulla amet fugiat nostrud sint nulla aute est. ")),
+            React.createElement("article", null, 
+                React.createElement("h3", null, "Item Three"),
+                React.createElement("p", null, "Nulla amet fugiat nostrud sint nulla aute est. ")),
+            React.createElement("article", null, 
+                React.createElement("h3", null, "Item Four"),
+                React.createElement("p", null, "Nulla amet fugiat nostrud sint nulla aute est. ")))
+    )
+)
 
 
 
-
-ReactDOM.render(landingPage, document.getElementById("root"));
+ReactDOM.render(landingPage1, document.getElementById("root"));
