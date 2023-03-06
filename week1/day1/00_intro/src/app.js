b0cef339430d21d4c3e6a2746b15e0cce6f07c75
Kanako-muff <kanako.design00@gmail.com>
2023-03-06 12:44:25 -0800

    change code jsx to 'React.createElement

--- week1/day1/00_intro/src/app.js
+++ week1/day1/00_intro/src/app.js
@@ -114,7 +114,49 @@ const landingPage =
 
 // translate jsx code to react.createElement code.
 // code here 
-
+let jxlTranspile = React.createElement("div", {className: "container"},
+React.createElement("nav", null,
+    React.createElement("ul", null,
+    React.createElement("li", null,
+        React.createElement("h3", null, "LOGO")
+        ),
+        React.createElement("li", null,
+            React.createElement("a", {href: "#home"}, "Home")
+            ),
+        React.createElement("li", null,
+            React.createElement("a", {href: "#about"}, "About")
+            ),
+        React.createElement("li", null,
+            React.createElement("a", {href: "#contact"}, "Contact")
+            )
+    )
+),
+React.createElement("main", null,
+    React.createElement("section", null,
+        React.createElement("h1", {id:"title"}, "Hello World"),
+        React.createElement("p", {id:"paragraph"}, "This is first React Application"),
+        React.createElement("button", {className: "btn btn-dark"}, "Click me"),
+    ),
+    React.createElement("section", {id: "article"},
+        React.createElement("article", null,
+            React.createElement("h3", null, "Item One"),
+            React.createElement("p", null, "Nulla amet........."),
+            ),
+        React.createElement("article", null,
+            React.createElement("h3", null, "Item One"),
+            React.createElement("p", null, "Nulla amet........."),
+            ),
+        React.createElement("article", null,
+            React.createElement("h3", null, "Item One"),
+            React.createElement("p", null, "Nulla amet........."),
+            ),
+        React.createElement("article", null,
+            React.createElement("h3", null, "Item One"),
+            React.createElement("p", null, "Nulla amet........."),
+            ),
+        ),
+    ),
+)
 
 
 
