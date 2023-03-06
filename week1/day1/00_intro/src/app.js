626475725f18e6c3a9056f07d525bbef4b1d23f8
Kubilay <kubilayckmk@gmail.com>
2023-03-06 11:47:33 -0800

    jsx and react.createElement

---
+++ week1/day1/00_intro/src/app.js
@@ -0,0 +1,121 @@
+// final name = "Joe";
+
+// let element = React.createElement("h1", null, "Hello React!");
+// console.log(element);
+
+/*
+{
+    type: "h1", 
+    key: null, 
+    ref: null, 
+    props: {
+        className: "null", 
+        children: "Hello React!"
+    }, 
+    _owner: null, 
+    _store: {}
+}
+ */
+
+// vanilla js to create element
+
+// const headTitle = document.createElement("h1");
+// headTitle.innerHTML = "Heading";
+// headTitle.addEventListener.apply.
+
+// React.createElement to create element on DOM.
+
+const myDiv = React.createElement(
+    "div",
+    { className: "my-class" },
+    React.createElement("h1", null, "Heading"),
+    React.createElement("p", null, "Lorem ipsum bla bla")
+);
+
+const NavBar = React.createElement(
+    "nav",
+    null,
+    React.createElement("ul", null, React.createElement("li", null, 
+    React.createElement("a", { href: "https://google.com"}, "Home"))),
+    React.createElement("ul", null, React.createElement("li", null, "Contact")),
+    React.createElement("ul", null, React.createElement("li", null, "About"))
+)
+
+function handleClick () {
+    console.log("Discovering...");
+}
+
+const Hero = React.createElement("div", { className: "container" }, 
+    React.createElement("h1", { className: "title" }, "Hello React"),
+    React.createElement("p", { className: "paragraph" }, "This is the way of using React.createElement"),
+    React.createElement("button", { onClick: handleClick }, "Discover")
+)
+
+const HeroJSX = <div className="container">
+    <h1 className="title">Hello React</h1>
+    <p className="paragraph">This is the way of using JSX</p>
+    <button onClick={handleClick}>Discover</button>
+</div>;
+
+// JSX way to create element on DOM.
+
+// let element = <div className="my-class">
+//     <h1>Heading 1</h1>    
+//     <p>Lorem ipsum bla bla</p>
+// </div>;
+
+// Render the element on Virtual React DOM.
+
+const landingPage = 
+<div className="container">
+    <nav>
+        <ul>
+            <li>
+                <h3>LOGO</h3>
+            </li>
+            <li>
+                <a href="#home">Home</a>
+            </li>
+            <li>
+                <a href="#about">About</a>
+            </li>
+            <li>
+                <a href="#contact">Contact</a>
+            </li>
+        </ul>
+    </nav>
+    <main>
+        <section>
+            <h1 id="title">Hello World</h1>
+            <p id="paragraph">This is first React Application</p>
+            <button className="btn btn-dark">Click me</button>
+        </section>
+
+        <section id="article">
+            <article>
+                <h3>Item One</h3>
+                <p>Nulla amet fugiat nostrud sint nulla aute est. </p>
+            </article>
+            <article>
+                <h3>Item Two</h3>
+                <p>Nulla amet fugiat nostrud sint nulla aute est. </p>
+            </article>
+            <article>
+                <h3>Item Three</h3>
+                <p>Nulla amet fugiat nostrud sint nulla aute est. </p>
+            </article>
+            <article>
+                <h3>Item Four</h3>
+                <p>Nulla amet fugiat nostrud sint nulla aute est. </p>
+            </article>
+        </section>
+    </main>
+</div>
+
+// translate jsx code to react.createElement code.
+// code here 
+
+
+
+
+ReactDOM.render(landingPage, document.getElementById("root"));
