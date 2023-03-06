b070f782a5ca96510e9f4cca8801e35dcf8af3bf
Aya <pyon220@gmail.com>
2023-03-06 12:40:01 -0800

    exercise finished

---
+++ week1/day1/00_intro/src/answer.js
@@ -0,0 +1,104 @@
+const landingPage = React.createElement(
+  "div",
+  { className: "container" },
+  React.createElement(
+    nav,
+    null,
+    React.createElement(
+      ul,
+      null,
+      React.createElement(li, null, React.createElement(h3, null, "LOGO"))
+    )
+  )
+);
+//   <div className="container">
+//     <nav>
+//       <ul>
+//         <li>
+//           <h3>LOGO</h3>
+//         </li>
+//         <li>
+//           <a href="#home">Home</a>
+//         </li>
+//         <li>
+//           <a href="#about">About</a>
+//         </li>
+//         <li>
+//           <a href="#contact">Contact</a>
+//         </li>
+//       </ul>
+//     </nav>
+//     <main>
+//       <section>
+//         <h1 id="title">Hello World</h1>
+//         <p id="paragraph">This is first React Application</p>
+//         <button className="btn btn-dark">Click me</button>
+//       </section>
+
+//       <section id="article">
+//         <article>
+//           <h3>Item One</h3>
+//           <p>Nulla amet fugiat nostrud sint nulla aute est. </p>
+//         </article>
+//         <article>
+//           <h3>Item Two</h3>
+//           <p>Nulla amet fugiat nostrud sint nulla aute est. </p>
+//         </article>
+//         <article>
+//           <h3>Item Three</h3>
+//           <p>Nulla amet fugiat nostrud sint nulla aute est. </p>
+//         </article>
+//         <article>
+//           <h3>Item Four</h3>
+//           <p>Nulla amet fugiat nostrud sint nulla aute est. </p>
+//         </article>
+//       </section>
+//     </main>
+//   </div>
+// );
+// const landingPage =
+// <div className="container">
+//     <nav>
+//         <ul>
+//             <li>
+//                 <h3>LOGO</h3>
+//             </li>
+//             <li>
+//                 <a href="#home">Home</a>
+//             </li>
+//             <li>
+//                 <a href="#about">About</a>
+//             </li>
+//             <li>
+//                 <a href="#contact">Contact</a>
+//             </li>
+//         </ul>
+//     </nav>
+//     <main>
+//         <section>
+//             <h1 id="title">Hello World</h1>
+//             <p id="paragraph">This is first React Application</p>
+//             <button className="btn btn-dark">Click me</button>
+//         </section>
+
+//         <section id="article">
+//             <article>
+//                 <h3>Item One</h3>
+//                 <p>Nulla amet fugiat nostrud sint nulla aute est. </p>
+//             </article>
+//             <article>
+//                 <h3>Item Two</h3>
+//                 <p>Nulla amet fugiat nostrud sint nulla aute est. </p>
+//             </article>
+//             <article>
+//                 <h3>Item Three</h3>
+//                 <p>Nulla amet fugiat nostrud sint nulla aute est. </p>
+//             </article>
+//             <article>
+//                 <h3>Item Four</h3>
+//                 <p>Nulla amet fugiat nostrud sint nulla aute est. </p>
+//             </article>
+//         </section>
+//     </main>
+// </div>
+ReactDOM.render(landingPage, document.getElementById("root"));
