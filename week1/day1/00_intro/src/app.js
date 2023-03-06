c0601060a5c8725b959e00e33320f2b314677581
Alondra Sanchez <alomonse1426@gmail.com>
2023-03-06 12:26:07 -0800

    Âdone kubilayzito

--- week1/day1/00_intro/src/app.js
+++ week1/day1/00_intro/src/app.js
@@ -35,17 +35,17 @@ const myDiv = React.createElement(
 const NavBar = React.createElement(
     "nav",
     null,
-    React.createElement("ul", null, React.createElement("li", null, 
-    React.createElement("a", { href: "https://google.com"}, "Home"))),
+    React.createElement("ul", null, React.createElement("li", null,
+        React.createElement("a", { href: "https://google.com" }, "Home"))),
     React.createElement("ul", null, React.createElement("li", null, "Contact")),
     React.createElement("ul", null, React.createElement("li", null, "About"))
 )
 
-function handleClick () {
+function handleClick() {
     console.log("Discovering...");
 }
 
-const Hero = React.createElement("div", { className: "container" }, 
+const Hero = React.createElement("div", { className: "container" },
     React.createElement("h1", { className: "title" }, "Hello React"),
     React.createElement("p", { className: "paragraph" }, "This is the way of using React.createElement"),
     React.createElement("button", { onClick: handleClick }, "Discover")
@@ -66,54 +66,93 @@ const HeroJSX = <div className="container">
 
 // Render the element on Virtual React DOM.
 
-const landingPage = 
-<div className="container">
-    <nav>
-        <ul>
-            <li>
-                <h3>LOGO</h3>
-            </li>
-            <li>
-                <a href="#home">Home</a>
-            </li>
-            <li>
-                <a href="#about">About</a>
-            </li>
-            <li>
-                <a href="#contact">Contact</a>
-            </li>
-        </ul>
-    </nav>
-    <main>
-        <section>
-            <h1 id="title">Hello World</h1>
-            <p id="paragraph">This is first React Application</p>
-            <button className="btn btn-dark">Click me</button>
-        </section>
+const landingPage =
+    <div className="container">
+        <nav>
+            <ul>
+                <li>
+                    <h3>LOGO</h3>
+                </li>
+                <li>
+                    <a href="#home">Home</a>
+                </li>
+                <li>
+                    <a href="#about">About</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact</a>
+                </li>
+            </ul>
+        </nav>
+        <main>
+            <section>
+                <h1 id="title">Hello World</h1>
+                <p id="paragraph">This is first React Application</p>
+                <button className="btn btn-dark">Click me</button>
+            </section>
 
-        <section id="article">
-            <article>
-                <h3>Item One</h3>
-                <p>Nulla amet fugiat nostrud sint nulla aute est. </p>
-            </article>
-            <article>
-                <h3>Item Two</h3>
-                <p>Nulla amet fugiat nostrud sint nulla aute est. </p>
-            </article>
-            <article>
-                <h3>Item Three</h3>
-                <p>Nulla amet fugiat nostrud sint nulla aute est. </p>
-            </article>
-            <article>
-                <h3>Item Four</h3>
-                <p>Nulla amet fugiat nostrud sint nulla aute est. </p>
-            </article>
-        </section>
-    </main>
-</div>
+            <section id="article">
+                <article>
+                    <h3>Item One</h3>
+                    <p>Nulla amet fugiat nostrud sint nulla aute est. </p>
+                </article>
+                <article>
+                    <h3>Item Two</h3>
+                    <p>Nulla amet fugiat nostrud sint nulla aute est. </p>
+                </article>
+                <article>
+                    <h3>Item Three</h3>
+                    <p>Nulla amet fugiat nostrud sint nulla aute est. </p>
+                </article>
+                <article>
+                    <h3>Item Four</h3>
+                    <p>Nulla amet fugiat nostrud sint nulla aute est. </p>
+                </article>
+            </section>
+        </main>
+    </div>
 
 // translate jsx code to react.createElement code.
 // code here 
+
+const landingPageReact = React.createElement("div", { className: "container" },
+    React.createElement("nav", null, React.createElement("ul", null,
+        React.createElement("li", null, React.createElement(("h3", null,
+            React.createElement("a", { href: "#home" }, "Home"))),
+            React.createElement("li", null,
+                React.createElement("a", { href: "#about" }, "About"))),
+        React.createElement("li", null,
+            React.createElement("a", { href: "#contact" }, "contact")))
+    ),
+
+    React.createElement("main", null,
+        React.createElement("section", null,
+            React.createElement("h1", { id: "title" }, "Hello world"),
+            React.createElement("p", { id: "paragraph" }, "This is first React Application"),
+            React.createElement("button", { classname: "btn-dark" }, "Click me")),
+
+        React.createElement("section", { id: "article" },
+
+            React.createElement("article", null,
+                React.createElement("h3", { id: "title" }, "Item one"),
+                React.createElement("p", null, "Nulla amet fugiat nostrud sint nulla aute est.")),
+
+            React.createElement("article", null,
+                React.createElement("h3", { id: "title" }, "Item Two"),
+                React.createElement("p", null, "Nulla amet fugiat nostrud sint nulla aute est.")),
+
+            React.createElement("article", null,
+                React.createElement("h3", { id: "title" }, "Item Three"),
+                React.createElement("p", null, "Nulla amet fugiat nostrud sint nulla aute est.")),
+
+            React.createElement("article", null,
+                React.createElement("h3", { id: "title" }, "Item Four"),
+                React.createElement("p", null, "Nulla amet fugiat nostrud sint nulla aute est."))
+        )
+    )
+)
+
+
 
 
 
