110eff6f6dc00ac07909525a921aa9284638a5e7
Maho Ando <snoopy3janice@gmail.com>
2023-03-06 19:22:36 -0800

    done to translate jsx code to react.createElement code

--- week1/day1/00_intro/src/app.js
+++ week1/day1/00_intro/src/app.js
@@ -114,8 +114,51 @@ const landingPage =
 
 // translate jsx code to react.createElement code.
 // code here 
+const pTagText = "Nulla amet fugiat nostrud sint nulla aute est. ";
 
+const landingPage1 = React.createElement("div", {className: "container"},
+    React.createElement("nav", null,
+        React.createElement("ul", null,
+            React.createElement("li", null,
+                React.createElement("h3", "LOGO")
+            ),
+            React.createElement("li", null,
+                React.createElement("a", { href: "#home"}, "Home")
+            ),
+            React.createElement("li", null,
+                React.createElement("a", { href: "#about"}, "About")
+            ),
+            React.createElement("li", null,
+                React.createElement("a", { href: "#contact"}, "Contact")
+            )
+        )
+    ),
+    React.createElement("main", null,
+        React.createElement("section", null,
+            React.createElement("h1", {id: "title"}, "Hello World"),
+            React.createElement("p", {id: "paragraph"}, "This is first React Application"),
+            React.createElement("button", {className: "btn btn-dark"}, "Click me")
+        ),
+        React.createElement("section", {id: "article"},
+            React.createElement("article", null,
+                React.createElement("h3", null, "Item One"),
+                React.createElement("p", null, pTagText)
+            ),
+            React.createElement("article", null,
+                React.createElement("h3", null, "Item Two"),
+                React.createElement("p", null, pTagText)
+            ),
+            React.createElement("article", null,
+                React.createElement("h3", null, "Item Three"),
+                React.createElement("p", null, pTagText)
+            ),
+            React.createElement("article", null,
+                React.createElement("h3", null, "Item Four"),
+                React.createElement("p", null, pTagText)
+            )
+        )
+    )
+)
 
-
-
-ReactDOM.render(landingPage, document.getElementById("root"));
+// ReactDOM.render(landingPage, document.getElementById("root"));
+ReactDOM.render(landingPage1, document.getElementById("root"));
