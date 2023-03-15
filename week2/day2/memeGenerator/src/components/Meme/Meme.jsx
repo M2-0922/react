69611aa5137518d24667dde917e5b5645e553ca5
Kubilay <kubilayckmk@gmail.com>
2023-03-15 09:58:15 -0700

    memeGenerator v2

--- week2/day2/memeGenerator/src/components/Meme/Meme.jsx
+++ week2/day2/memeGenerator/src/components/Meme/Meme.jsx
@@ -5,14 +5,36 @@ const Meme = () => {
     // const [topText, setTopText] = useState("");
     // const [bottomText, setBottomText] = useState("");
     // const [randomImage, setRandomImage] = useState("");
-
     const [meme, setMeme] = useState(
         {
             topText: "",
             bottomText: "",
             randomImage: defaultImage
         }
-    )
+    );
+    const [allMemes, setAllMemes] = useState([]);
+    
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => {
+            setAllMemes(data.data.memes);
+        })
+    }, []);
+
+    const getMemeImage = () => {
+        const memeArr = allMemes;
+        const randomNumber = Math.floor(Math.random() * memeArr.length);
+        const imageUrl = memeArr[randomNumber].url;
+
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                randomImage: imageUrl
+            }
+        })
+    }
+
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -29,6 +51,7 @@ const Meme = () => {
         <main>
             <div className={style.form}>
                 <input 
+                    className={`${style.customInput}`}
                     type="text" 
                     placeholder="Top Text"
                     name="topText"
@@ -36,17 +59,18 @@ const Meme = () => {
                     // onChange={(event) => handleChange(event)}
                 />
                 <input 
+                    className={`${style.customInput}`}
                     type="text"
                     placeholder="Bottom Text"
                     name="bottomText"
                     onChange={handleChange}
                  />
-                <button>Get a new meme image</button>
+                <button className={`${style.customButton}`} onClick={getMemeImage}>Get a new meme image 🖼</button>
             </div>
             <div className={style.meme}>
-                <img src={meme.randomImage} alt="random-meme-image" />
-                <h2>{meme.topText}</h2>
-                <h2>{meme.bottomText}</h2>
+                <img className={style.memeImage} src={meme.randomImage} alt="random-meme-image" />
+                <h2 className={`${style.topText} ${style.content}`}>{meme.topText}</h2>
+                <h2 className={`${style.bottomText} ${style.content}`}>{meme.bottomText}</h2>
             </div>
         </main>
     )
