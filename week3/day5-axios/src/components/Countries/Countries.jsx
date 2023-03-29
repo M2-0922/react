50650b30b26ec4ae2a8e962246979099483c81a1
Maho Ando <snoopy3janice@gmail.com>
2023-03-28 21:04:31 -0700

    edit counries and joke file

--- week3/day5-axios/src/components/Countries/Countries.jsx
+++ week3/day5-axios/src/components/Countries/Countries.jsx
@@ -3,36 +3,54 @@ import axios from "axios";
 import { 
     Container,
     ListGroup,
-    ListGroupItem
+    ListGroupItem,
+    Input,
+    Label
 } from "reactstrap"
 
 const Countries = () => {
-  const [countries, setCountries] = useState([]);
+    const [countries, setCountries] = useState([]);
+    const [filtered, setFiltered] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+    const searchInputValue = event => setInputValue(event.target.value);
 
-  useEffect(() => {
-    const fetchCountries = async () => {
-        try {
-            const response = await axios.get("https://restcountries.com/v3.1/all");
-            setCountries(response.data);
-        } catch (error) {
-            console.log(`Error while fetching api: ${error}`);
-        }
-    }
-    fetchCountries();
-  }, [])
-
-  return (
-    <Container>
-        <h1>Countries</h1>
-        <ListGroup>
-            {
-                countries.map((country) => {
-                    return <ListGroupItem key={country.cca3}>{country.name.common} {country.flag} - {country.capital}</ListGroupItem>
-                })
+    //Only first time onload
+    useEffect(() => {
+        const fetchCountries = async () => {
+            try {
+                const response = await axios.get("https://restcountries.com/v3.1/all");
+                setCountries(response.data);
+            } catch (error) {
+                console.log(`Error while fetching api: ${error}`);
             }
-        </ListGroup>
-    </Container>
-  )
+        }
+        fetchCountries();
+    }, []);
+
+    // check input value and api data
+    useEffect(() => {
+        setFiltered(
+            countries.filter(country => {
+                const regexValue = new RegExp(`^${inputValue}`, 'gi')
+                return regexValue.test(country.name.common);
+            })
+        )
+    }, [countries, inputValue]);
+
+    return (
+        <Container>
+            <h1>Countries</h1>
+            <ListGroup>
+                <Label for="search">Search Country </Label>
+                <Input type="text" id="search" onChange={searchInputValue} value={inputValue} />
+                    {
+                        filtered.map((country) => {
+                            return <ListGroupItem key={country.cca3}>{country.name.common} {country.flag} - {country.capital}</ListGroupItem>
+                        })
+                    }
+            </ListGroup>
+        </Container>
+    )
 }
 
 export default Countries
--- week3/day5-axios/src/components/Joke/Joke.jsx
+++ week3/day5-axios/src/components/Joke/Joke.jsx
@@ -1,31 +1,61 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from "axios";
 import {
     Container,
     Button,
-    Alert
+    Alert,
+    Input,
+    Label
 } from "reactstrap";
 
 const Joke = () => {
     const [joke, setJoke] = useState('');
+    const [categories, setCategories] = useState([]);
+    const [category, setCategory] = useState('');
+    const searchSelectedValue = event => setCategory(event.target.value);
 
+    //get a joke from api by key
     const fetchJoke = async () => {
         try {
-            const response = await axios.get("https://v2.jokeapi.dev/joke/Any?type=single");
+            const response = await axios.get(`https://v2.jokeapi.dev/joke/${category}?type=single`);
             setJoke(response.data);
         } catch (error) {
             console.log(`Error while fetching: ${error}`);
         }
     }
 
-  return (
-    <Container>
-        <h1>Random Joke</h1>
-        <Alert color='warning'>{joke.joke}</Alert>
-        <Alert color='warning'>Category: {joke.category}</Alert>
-        <Button color='warning' onClick={fetchJoke}>Get a new Joke</Button>
-    </Container>
-  )
+    //Only first time onload
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const response = await axios.get("https://v2.jokeapi.dev/categories");
+                setCategories(response.data.categories);
+            } catch (error) {
+                console.log(`Error while fetching api: ${error}`);
+            }
+        }
+        fetchCategories();
+    }, []);
+
+    //set select pulldown
+    useEffect(() => {
+        setCategory(categories[0])}, [categories]
+    );
+
+    return (
+        <Container>
+            <h1>Random Joke</h1>
+            <Label for="select">Category </Label>
+            <Input type="select" id="select" onChange={searchSelectedValue}>
+                {
+                    categories.map(category => <option key={category} value={category}>{category}</option>)
+                }
+            </Input>
+            <Alert color='warning'>{joke.joke}</Alert>
+            <Alert color='warning'>Category: {joke.category}</Alert>
+            <Button color='warning' onClick={fetchJoke}>Get a new Joke</Button>
+        </Container>
+    )
 }
 
 export default Joke
