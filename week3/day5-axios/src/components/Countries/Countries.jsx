f497733af7b4bb9294a81cf56adee54fb7dbca20
koshirok096 <koshirok096@gmail.com>
2023-04-01 01:12:14 -0700

    homework

--- week3/day5-axios/src/components/Countries/Countries.jsx
+++ week3/day5-axios/src/components/Countries/Countries.jsx
@@ -3,11 +3,13 @@ import axios from "axios";
 import { 
     Container,
     ListGroup,
-    ListGroupItem
+    ListGroupItem,
+    Input
 } from "reactstrap"
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     const fetchCountries = async () => {
@@ -21,12 +23,21 @@ const Countries = () => {
     fetchCountries();
   }, [])
 
+  const handleSearchTermChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredCountries = countries.filter((country) => {
+    return country.name.common.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
   return (
     <Container>
         <h1>Countries</h1>
+        <Input type="text" placeholder="Search" value={searchTerm} onChange={handleSearchTermChange} />
         <ListGroup>
             {
-                countries.map((country) => {
+                filteredCountries.map((country) => {
                     return <ListGroupItem key={country.cca3}>{country.name.common} {country.flag} - {country.capital}</ListGroupItem>
                 })
             }
@@ -36,3 +47,43 @@ const Countries = () => {
 }
 
 export default Countries
+
+
+// import React, { useState, useEffect } from 'react'
+// import axios from "axios";
+// import { 
+//     Container,
+//     ListGroup,
+//     ListGroupItem
+// } from "reactstrap"
+
+// const Countries = () => {
+//   const [countries, setCountries] = useState([]);
+
+//   useEffect(() => {
+//     const fetchCountries = async () => {
+//         try {
+//             const response = await axios.get("https://restcountries.com/v3.1/all");
+//             setCountries(response.data);
+//         } catch (error) {
+//             console.log(`Error while fetching api: ${error}`);
+//         }
+//     }
+//     fetchCountries();
+//   }, [])
+
+//   return (
+//     <Container>
+//         <h1>Countries</h1>
+//         <ListGroup>
+//             {
+//                 countries.map((country) => {
+//                     return <ListGroupItem key={country.cca3}>{country.name.common} {country.flag} - {country.capital}</ListGroupItem>
+//                 })
+//             }
+//         </ListGroup>
+//     </Container>
+//   )
+// }
+
+// export default Countries
--- week3/day5-axios/src/components/Joke/Joke.jsx
+++ week3/day5-axios/src/components/Joke/Joke.jsx
@@ -1,31 +1,115 @@
-import React, { useState } from 'react'
-import axios from "axios";
-import {
-    Container,
-    Button,
-    Alert
-} from "reactstrap";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Container, Button, Alert } from 'reactstrap';
 
 const Joke = () => {
-    const [joke, setJoke] = useState('');
+  const [joke, setJoke] = useState('');
+  const [category, setCategory] = useState('');
 
-    const fetchJoke = async () => {
-        try {
-            const response = await axios.get("https://v2.jokeapi.dev/joke/Any?type=single");
-            setJoke(response.data);
-        } catch (error) {
-            console.log(`Error while fetching: ${error}`);
-        }
+  useEffect(() => {
+    if (category !== '') {
+      fetchJokeByCategory();
+    } 
+    else {
+      fetchJoke();
     }
+  }, [category]);
+
+  const fetchJokeByCategory = async () => {
+    try {
+        if (category !== 'Any'){
+        const response = await axios.get(
+            `https://v2.jokeapi.dev/joke/${category}?type=single`
+            );
+            setJoke(response.data);        
+        } else {
+            const response = await axios.get(
+                `https://v2.jokeapi.dev/joke/Any?type=single`
+                );
+                setJoke(response.data);        
+            }
+    } catch (error) {
+      console.log(`Error while fetching: ${error}`);
+    }
+  };
+
+  const fetchJoke = async () => {
+    try {
+    const response = await axios.get(
+        'https://v2.jokeapi.dev/joke/Any?type=single'
+    );
+    setJoke(response.data);
+    } catch (error) {
+    console.log(`Error while fetching: ${error}`);
+    }
+  };
+
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  };
 
   return (
     <Container>
-        <h1>Random Joke</h1>
-        <Alert color='warning'>{joke.joke}</Alert>
+      <h1>Random Joke</h1>
+      <Alert color='warning'>{joke.joke}</Alert>
+      {joke.category && (
         <Alert color='warning'>Category: {joke.category}</Alert>
-        <Button color='warning' onClick={fetchJoke}>Get a new Joke</Button>
+      )}
+      <div>
+        <label>Filter by category: </label>
+        <select value={category} onChange={handleCategoryChange}>
+          <option value=''>Any</option>
+          <option value='Programming'>Programming</option>
+          <option value='Misc'>Misc</option>
+          <option value='Dark'>Dark</option>
+          <option value='Pun'>Pun</option>
+          <option value='Spooky'>Spooky</option>
+          <option value='Christmas'>Christmas</option>
+        </select>
+      </div>
+      <Button color='warning' onClick={fetchJokeByCategory}>
+        Get a new Joke
+      </Button>
     </Container>
-  )
-}
+  );
+};
 
-export default Joke
+export default Joke;
+
+
+
+
+
+
+// OG
+// import React, { useState } from 'react'
+// import axios from "axios";
+// import {
+//     Container,
+//     Button,
+//     Alert
+// } from "reactstrap";
+
+// const Joke = () => {
+//     const [joke, setJoke] = useState('');
+
+//     const fetchJoke = async () => {
+//         try {
+//             const response = await axios.get("https://v2.jokeapi.dev/joke/Any?type=single");
+//             setJoke(response.data);
+//         } catch (error) {
+//             console.log(`Error while fetching: ${error}`);
+//         }
+//     }
+
+//   return (
+//     <Container>
+//         <h1>Random Joke</h1>
+//         <Alert color='warning'>{joke.joke}</Alert>
+//         <Alert color='warning'>Category: {joke.category}</Alert>
+//         <Button color='warning' onClick={fetchJoke}>Get a new Joke</Button>
+//     </Container>
+//   )
+// }
+
+// export default Joke
