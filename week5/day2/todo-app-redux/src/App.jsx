114d0bd7dffd02fd74a3385415c86f0f8e6e4b0c
Maho Ando <snoopy3janice@gmail.com>
2023-04-16 18:53:20 -0700

    Add edit todo to our project

--- week5/day2/todo-app-redux/src/App.jsx
+++ week5/day2/todo-app-redux/src/App.jsx
@@ -1,49 +1,57 @@
-import './App.css'
-import { useRef } from "react"
-import { useSelector, useDispatch } from 'react-redux';
-import { v4 as uuidv4 } from "uuid"
-import { addTodo, deleteTodo, toggleTodo, updateTodo } from "./actions/actions"
+import "./App.css";
+import { useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
+import {
+  addTodo,
+  deleteTodo,
+  toggleTodo,
+  updateTodo,
+  editTodo,
+  submitTodo,
+} from "./actions/actions";
 
 function App() {
-  const todos = useSelector(state => state.todos);
+  const todos = useSelector((state) => state.todos);
   const inputRef = useRef(null);
+  const inputTextRef = useRef(null);
   const dispatch = useDispatch();
 
   const handleClick = () => {
     // dispatch(
-      // { type: "ADD_TODO", 
-      //   payload: { 
-      //     id: uuidv4(), 
-      //     text: inputRef.current.value, 
-      //     completed: false 
-      //   } 
-      // }
+    // { type: "ADD_TODO",
+    //   payload: {
+    //     id: uuidv4(),
+    //     text: inputRef.current.value,
+    //     completed: false
+    //   }
+    // }
     // )
 
     const data = {
-      id: uuidv4(), 
-      text: inputRef.current.value, 
-      completed: false 
+      id: uuidv4(),
+      text: inputRef.current.value,
+      completed: false,
     };
 
-    dispatch(addTodo(data))
+    dispatch(addTodo(data));
     inputRef.current.value = "";
-  }
+  };
 
   const handleDelete = (id) => {
-    dispatch(deleteTodo(id))
-  }
+    dispatch(deleteTodo(id));
+  };
 
   const handleToggle = (id) => {
     dispatch(toggleTodo(id));
-  }
+  };
 
   // write a function for edit todo to dispatch to reducer
 
   // const handleUpdate = (id, text, completed) => {
   //   console.log(id, text, completed);
   //   const data = {
-  //     id, 
+  //     id,
   //     text: text + " updated",
   //     completed
   //   }
@@ -51,37 +59,60 @@ function App() {
   //   dispatch(updateTodo(data));
   // }
 
+  const handleEdit = (id) => {
+    dispatch(editTodo(id));
+  };
+
+  const handleSubmit = (id) => {
+    const newText = inputTextRef.current.value;
+    dispatch(submitTodo(id, newText));
+    inputTextRef.current.value = "";
+  };
+
   return (
     <div className="App">
       <input type="text" ref={inputRef} />
       <button onClick={handleClick}>Add</button>
-      {
-        todos.map((todo) => {
-          return (
-            <div key={todo.id}>
-              <li className='todo'>
-                <input 
-                  type="checkbox" 
-                  checked={todo.completed}
-                  onChange={() => handleToggle(todo.id)}
-                />
-                <b className={todo.completed ? 'completed-todo': ""}>{todo.text}</b>
-                {/* once you click to edit button, its going to have an input instead of text and once you type and submit, you need to dispatch to reducer. */}
-                {/* <button 
-                  onClick={() => handleUpdate(todo.id, todo.text, todo.completed)}>
-                    Update
-                  </button> */}
-                <button 
-                  onClick={() => handleDelete(todo.id)}>
-                    Delete
+      {todos.map((todo) => {
+        return (
+          <div key={todo.id}>
+            <li className="todo">
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => handleToggle(todo.id)}
+              />
+              {!todo.inputTable ? (
+                <>
+                  <b className={todo.completed ? "completed-todo" : ""}>
+                    {todo.text}
+                  </b>
+                  {/* once you click to edit button, its going to have an input instead of text and once you type and submit, you need to dispatch to reducer. */}
+                  {/* <button 
+                      onClick={() => handleUpdate(todo.id, todo.text, todo.completed)}>
+                        Update
+                      </button> */}
+                  <button onClick={() => handleEdit(todo.id, todo.text)}>
+                    Edit
                   </button>
-              </li>
-            </div>
-          )
-        }) 
-      }
+                </>
+              ) : (
+                <>
+                  <input
+                    type="text"
+                    defaultValue={todo.text}
+                    ref={inputTextRef}
+                  />
+                  <button onClick={() => handleSubmit(todo.id)}>Submit</button>
+                </>
+              )}
+              <button onClick={() => handleDelete(todo.id)}>Delete</button>
+            </li>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- week5/day2/todo-app-redux/src/reducers/rootReducer.js
+++ week5/day2/todo-app-redux/src/reducers/rootReducer.js
@@ -1,48 +1,49 @@
 const initialState = {
-    todos:[]
+todos: [],
 };
 
-function rootReducer(state = initialState, action){
-    switch (action.type) {
-        case 'ADD_TODO':
-            return {
-                todos: [...state.todos, action.payload]
+function rootReducer(state = initialState, action) {
+switch (action.type) {
+    case "ADD_TODO":
+        return {
+            todos: [...state.todos, action.payload],
+        };
+    case "DELETE_TODO":
+        return {
+            todos: state.todos.filter((todo) => todo.id != action.payload.id),
+        };
+    case "TOGGLE_TODO":
+        return {
+            todos: state.todos.map((todo) => {
+            if (todo.id === action.payload.id) {
+                return {
+                ...todo,
+                completed: !todo.completed,
+                };
+            } else {
+                return todo;
             }
-        case 'DELETE_TODO':
-            return {
-                todos: state.todos.filter(todo => todo.id != action.payload.id)
-            }
-        case 'TOGGLE_TODO':
-            return {
-                todos: state.todos.map(todo => {
-                    if(todo.id === action.payload.id){
-                        return {
-                            ...todo, completed: !todo.completed
-                        }
-                    }else{
-                        return todo
-                    }
-                })
-            }
-        // case "UPDATE_TODO":
-        //     return {
-        //         todos: state.todos.map(todo => {
-        //             if(todo.id === action.payload.id){
-        //                 return { 
-        //                     id: action.payload.id,
-        //                     text: action.payload.text,
-        //                     completed: action.payload.completed
-        //                 }
-        //             }else {
-        //                 return todo
-        //             }
-        //         })
-        //     }
-
-        // write a case for edit todo with logic
-        default:
-            return state;
-    }
+            }),
+        };
+    // write a case for edit todo with logic
+    case "EDIT_TODO":
+        return {
+            todos: state.todos.map((todo) => ({
+            ...todo,
+            inputTable: todo.id === action.payload.id && true,
+            })),
+        };
+    case "SUBMIT_TODO":
+        return {
+            todos: state.todos.map((todo) => ({
+            ...todo,
+            text: todo.id === action.payload.id ? action.payload.text : todo.text,
+            inputTable: todo.id === action.payload.id && false,
+            })),
+        };
+    default:
+        return state;
+}
 }
 
 export default rootReducer;
